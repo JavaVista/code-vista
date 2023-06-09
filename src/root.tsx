c24849217812bb8400312071fac4830dcacb41ab
Javier 'Javi' Carrion <javier.carrion@designinteractive.net>
2023-06-08 21:03:37 -0400

    feat: :sparkles: add axios lib & post cookies to backend

--- src/root.tsx
+++ src/root.tsx
@@ -15,6 +15,9 @@ import './global.css';
 import { supabase } from './utils/supabase';
 import type { User } from './types/user.type';
 import { UserSessionContext } from './context/user-session';
+import type { Session } from 'supabase-auth-helpers-qwik';
+import type { AuthChangeEvent } from '@supabase/supabase-js';
+import axios from 'axios';
 
 export default component$(() => {
     /**
@@ -30,16 +33,36 @@ export default component$(() => {
 
     useVisibleTask$(async () => {
         const { data: authListener } = supabase.auth.onAuthStateChange(
-            async (event: string, session: any) => {
+            async (event: AuthChangeEvent, session: Session | null) => {
                 console.log('event: ', event);
                 console.log('session: ', session);
 
-                if (event === 'SIGNED_IN') {
+                if (
+                    event === 'SIGNED_IN' &&
+                    session?.access_token &&
+                    session?.refresh_token
+                ) {
                     // send cookie to server
+                    const body = {
+                        accessToken: session.access_token,
+                        refreshToken: session.refresh_token,
+                    };
 
-                    // set Auth state Context
-                    userSession.userId = session?.user?.id;
-                    userSession.isLoggedIn = true;
+                    // send request to server
+                    await axios
+                        .post('/api_v1/store-auth', body, {
+                            withCredentials: true,
+                        })
+                        .then(res => {
+                            console.log(res);
+
+                            // set Auth state Context
+                            userSession.userId = session?.user?.id;
+                            userSession.isLoggedIn = true;
+                        })
+                        .catch(err => {
+                            console.log(err);
+                        });
                 }
 
                 if (event === 'SIGNED_OUT') {
